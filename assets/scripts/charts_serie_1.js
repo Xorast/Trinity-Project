bb7c273add0806d48d2784a27d71da3dda218919
Xavier <xavier.astor.fontaine@gmail.com>
2018-05-08 22:18:24 +0000

    Added Temperature to Chart I.A Baseflow

--- assets/scripts/charts_serie_1.js
+++ assets/scripts/charts_serie_1.js
@@ -46,37 +46,73 @@ function makeGraph(error, inputData) {
         var maxDate = dimDate.top(1)[0].date;
         
     let dimTotalFlow = dimDate.group().reduceSum(dc.pluck("q"));
-    console.log(dimTotalFlow.top(1)[0]);
-    console.log(dimTotalFlow.all())
+    
+    let dimRain = dimDate.group().reduceSum(dc.pluck("rain"));
 
-    let chartBaseFlow = dc.compositeChart("#chart_I_A");
+    let chartBaseFlow  = dc.compositeChart("#chart_I_A");
     
     chartBaseFlow
         .width(1000)
-        .height(250)
+        .height(400)
         .dimension(dimDate)
         .x(d3.time.scale().domain([minDate,maxDate]))
+        .y(d3.scale.linear().domain([0,2]))
         .yAxisLabel("Flow (M3/DAY)")
+        .rightY(d3.scale.linear().domain([200,0]))
+        .rightYAxisLabel("Rain")
         .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
         .renderHorizontalGridLines(true)
         .renderVerticalGridLines(true)
+        .mouseZoomable(true)
         .compose([
             dc.lineChart(chartBaseFlow)
-            .colors("green")
-            .group(dimTotalFlow, "Flow - Total")
+                .colors("blue")
+                .group(dimTotalFlow, "Flow - Total")
+                .colors("blue")
+                .group(dimTotalFlow, "Flow - Total")
+                .colors("blue")
+                .group(dimTotalFlow, "Flow - Total")
+                .colors("blue")
+                .group(dimTotalFlow, "Flow - Total"),
+            dc.lineChart(chartBaseFlow)
+                .colors("red")
+                .group(dimRain, "Rain")
+                .useRightYAxis(true)
             ])
         .render()
 
-    // let chartBaseFlow = dc.lineChart("#chart_I_A");
-    
-    //     chartBaseFlow
-    //             .width(1000)
-    //             .height(300)
-    //             .dimension(dimDate)
-    //             .group(dimTotalFlow)
-    //             .x(d3.time.scale().domain([minDate,maxDate]))
-    //             .xAxisLabel("Date")
-    //             .y(d3.scale.linear().domain([0.00,1.50]));
+// -----------------------------------------------------------------------------------------
+// var chart = dc.boxPlot("#box-test"),
+//     pie = dc.pieChart("#pie-chart");
+// d3.csv("morley.csv").then(function(experiments) {
+//   experiments.forEach(function(x) {
+//     x.Speed = +x.Speed;
+//   });
+//   var ndx                 = crossfilter(experiments),
+//       runDimension        = ndx.dimension(function(d) {return +d.Run;}),
+//       runGroup            = runDimension.group(),
+//       experimentDimension = ndx.dimension(function(d) {return "exp-" + d.Expt;}),
+//       speedArrayGroup     = experimentDimension.group().reduce(
+//         function(p,v) {
+//           p.push(v.Speed);
+//           return p;
+//         },
+//         function(p,v) {
+//           p.splice(p.indexOf(v.Speed), 1);
+//           return p;
+//         },
+//         function() {
+//           return [];
+//         }
+//       );
+//   chart
+//     .width(768)
+//     .height(480)
+//     .margins({top: 10, right: 50, bottom: 30, left: 50})
+//     .dimension(experimentDimension)
+//     .group(speedArrayGroup)
+//     .elasticY(true)
+//     .elasticX(true);
 
 
 // END OF MAKEGRAPH
