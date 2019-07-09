f08b58205b92556cbd3484ec810836679d530784
pangur <pangur@tutanota.com>
2019-07-09 23:08:27 +0100

    Minor (typo)

--- data_tools.py
+++ data_tools.py
@@ -9,5 +9,5 @@ def relative_path(rel_file_path):
 def data_cleaning(value_as_string, dc_):
     """ Trim the float number stored as a string to `dc_` decimals. """
     if '.' in value_as_string:
-        value_as_string = value_as_string.split('.',1)[0] + '.' + value_as_string.split('.',1)[1][:dc_]
+        value_as_string = value_as_string.split('.', 1)[0] + '.' + value_as_string.split('.', 1)[1][:dc_]
     return value_as_string
